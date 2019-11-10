14456e9048a4b2f3a012cbfa2d0645b1e8b9db39
Jonas Jasas <jonas.jasas@gmail.com>
2019-11-10 08:54:35 +0200

    CORS

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -60,14 +60,14 @@ func corsHandler(h http.HandlerFunc, expose []string) http.HandlerFunc {
 }
 
 func cors(w http.ResponseWriter, r *http.Request, expose []string) {
+	origin := r.Header.Get("Origin")
+	if origin == "" {
+		origin = "*"
+	}
+	w.Header().Set("Access-Control-Allow-Origin", origin)
 	w.Header().Set("Httprelay-Version", Version)
 
 	if r.Method == "OPTIONS" {
-		origin := r.Header.Get("Origin")
-		if origin == "" {
-			origin = "*"
-		}
-		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	} else {
