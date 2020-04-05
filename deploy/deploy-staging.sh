da6b87bf0f3fd542df7d072ae8f5c78185905da2
Jonas Jasas <jonas.jasas@gmail.com>
2020-04-05 20:59:17 +0300

    Proxy method

---
+++ deploy/deploy-staging.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+scp -r ../* root@staging.httprelay.io:~/httprelay-stag
+ssh root@staging.httprelay.io "cd ~/httprelay-stag/deploy;
+COMPOSE_PROJECT_NAME=httprelay-stag SUB_DOMAIN=staging DOMAIN=httprelay.io docker-compose down
+COMPOSE_PROJECT_NAME=httprelay-stag SUB_DOMAIN=staging DOMAIN=httprelay.io docker-compose up --build"
