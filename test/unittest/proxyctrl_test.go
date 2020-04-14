f27b1682cc480b085eca5ddc90bfe5402ab0e95d
Jonas Jasas <jonas.jasas@gmail.com>
2020-04-14 08:41:06 +0300

    Proxy method tests

--- test/unittest/proxyctrl_test.go
+++ test/unittest/proxyctrl_test.go
@@ -3,6 +3,7 @@ package unittest
 import (
 	"gitlab.com/jonas.jasas/httprelay/pkg/controller"
 	"gitlab.com/jonas.jasas/httprelay/pkg/repository"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -10,8 +11,10 @@ import (
 	"testing"
 )
 
-var cliData = newString("Client data. ", 10000)
-var serData = newString("Server data. ", 10000)
+var cliData1 = newString("Client data 1. ", 10000)
+var cliData2 = newString("Client data 2. ", 10000)
+var serData1 = newString("Server data 1. ", 10000)
+var serData2 = newString("Server data 2. ", 10000)
 
 const serUrl = "https://domain/proxy/123/test"
 
@@ -20,47 +23,103 @@ func newReq(method string, url string, data string) (r *http.Request) {
 	return
 }
 
-func TestProxyCtrlConduct(t *testing.T) {
-	stopChan := make(chan struct{})
+func respDataEq(body io.Reader, data string) bool {
+	respData, _ := ioutil.ReadAll(body)
+	return string(respData) == data
+}
+
+func newCtrl() (proxyCtrl *controller.ProxyCtrl, stopChan chan struct{}, closeChan chan struct{}) {
+	stopChan = make(chan struct{})
 	proxyRep := repository.NewProxyRep()
-	proxyCtrl := controller.NewProxyCtrl(proxyRep, stopChan)
+	proxyCtrl = controller.NewProxyCtrl(proxyRep, stopChan)
+	closeChan = make(chan struct{})
+	return
+}
 
-	closeChan := make(chan struct{})
+func proxyCtrlCliReq(ctrl *controller.ProxyCtrl, data string) *httptest.ResponseRecorder {
+	r := newReq(http.MethodPost, serUrl, data)
+	w := httptest.NewRecorder()
+	ctrl.Conduct(w, r)
+	return w
+}
+func proxyCtrlSerReq(ctrl *controller.ProxyCtrl, data string, reqJobId string, wSecret string) (resp *httptest.ResponseRecorder, respJobId string) {
+	r := newReq("SERVE", serUrl, data)
+	r.Header.Add("httprelay-proxy-jobid", reqJobId)
+	r.Header.Add("httprelay-wsecret", wSecret)
+	w := httptest.NewRecorder()
+	ctrl.Conduct(w, r)
+	respJobId = w.Header().Get("httprelay-proxy-jobid")
+	return w, respJobId
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+func TestProxyCtrlConduct(t *testing.T) {
+	proxyCtrl, _, closeChan := newCtrl()
 	go func() {
-		proxyCtrlCliReq(t, proxyCtrl, closeChan)
+		defer close(closeChan)
+		resp := proxyCtrlCliReq(proxyCtrl, cliData1)
+		if !respDataEq(resp.Body, serData1) {
+			t.Error("Client received wrong response body")
+		}
 	}()
-
 	go func() {
-		proxyCtrlSerReqResp(t, proxyCtrl, closeChan)
+		defer close(closeChan)
+		resp, jobId := proxyCtrlSerReq(proxyCtrl, "", "", "")
+		if !respDataEq(resp.Body, cliData1) {
+			t.Error("Server received wrong client data body")
+			return
+		}
+		proxyCtrlSerReq(proxyCtrl, serData1, jobId, "")
 	}()
-
 	<-closeChan
 }
 
-func proxyCtrlCliReq(t *testing.T, ctrl *controller.ProxyCtrl, closeChan chan struct{}) {
-	r := newReq(http.MethodPost, serUrl, cliData)
-	w := httptest.NewRecorder()
-	ctrl.Conduct(w, r)
-	body, _ := ioutil.ReadAll(w.Body)
-	if string(body) != serData {
-		t.Error("Client received wrong response body")
-	}
-	close(closeChan)
-}
+func TestProxyCtrlWSecret(t *testing.T) {
+	proxyCtrl, _, closeChan := newCtrl()
+	go func() {
+		defer close(closeChan)
+		resp := proxyCtrlCliReq(proxyCtrl, cliData1)
+		if !respDataEq(resp.Body, serData1) {
+			t.Error("Client received wrong response body")
+			return
+		}
+		resp = proxyCtrlCliReq(proxyCtrl, cliData2)
+		if !respDataEq(resp.Body, serData2) {
+			t.Error("Client received wrong response body")
+			return
+		}
+	}()
 
-func proxyCtrlSerReqResp(t *testing.T, ctrl *controller.ProxyCtrl, closeChan chan struct{}) {
-	r1 := newReq("SERVE", serUrl, "")
-	w1 := httptest.NewRecorder()
-	ctrl.Conduct(w1, r1)
-	body, _ := ioutil.ReadAll(w1.Body)
-	if string(body) != cliData {
-		t.Error("Server received wrong client data body")
-		close(closeChan)
-		return
-	}
+	go func() {
+		const goodSecret = "secret1"
+		const badSecret = "bad secret"
+		defer close(closeChan)
+		resp, jobId := proxyCtrlSerReq(proxyCtrl, "", "", goodSecret)
+		if !respDataEq(resp.Body, cliData1) {
+			t.Error("Server received wrong client data body")
+			return
+		}
 
-	r2 := newReq("SERVE", serUrl, serData)
-	r2.Header.Add("httprelay-proxy-jobid", w1.Header().Get("httprelay-proxy-jobid"))
-	w2 := httptest.NewRecorder()
-	ctrl.Conduct(w2, r2)
+		resp, _ = proxyCtrlSerReq(proxyCtrl, serData1, jobId, badSecret)
+		if resp.Code != http.StatusUnauthorized {
+			t.Error("Server is accessing unauthorized data channel with the bad secret")
+			return
+		}
+
+		resp, jobId = proxyCtrlSerReq(proxyCtrl, serData1, jobId, goodSecret)
+		if !respDataEq(resp.Body, cliData2) {
+			t.Error("Server received wrong client data body")
+			return
+		}
+
+		resp, _ = proxyCtrlSerReq(proxyCtrl, serData1, jobId, badSecret)
+		if resp.Code != http.StatusUnauthorized {
+			t.Error("Server is accessing unauthorized data channel with the bad secret")
+			return
+		}
+
+		resp, jobId = proxyCtrlSerReq(proxyCtrl, serData2, jobId, goodSecret)
+	}()
+	<-closeChan
 }
