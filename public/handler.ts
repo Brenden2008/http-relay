93bfd0c2be06d973108823c447f6a9c32cd33389
Jonas Jasas <jonas.jasas@gmail.com>
2020-05-02 09:48:29 +0300

    CDN

--- public/handler.ts
+++ public/handler.ts
@@ -11,13 +11,15 @@ namespace HttpRelay.Proxy {
         signal: AbortSignal
     }
 
-
     export class Handler {
-        constructor(private readonly handlerFunc: HandlerFunc, private readonly abortSig: AbortSignal) {
-        }
+        constructor(
+            private readonly handlerFunc: HandlerFunc,
+            private readonly abortSig: AbortSignal,
+            private readonly wSecret: string
+        ) {}
 
 // Add these before fetch
-//    , private readonly abortSig: AbortSignal, wSecret: string
+//    , private readonly abortSig: AbortSignal
 //        if (wSecret) this.headers.set('httprelay-wsecret', wSecret)
 //        signal: this.abortSig
         execute(request: HandlerRequest, routeParams: string[]) {
@@ -32,42 +34,53 @@ namespace HttpRelay.Proxy {
 
             let defaultHeaders = new Headers()
             let defaultStatus = 200
-            let defaultContentType = 'application/json'
-            let defaultFileName = ''
+            let defaultContentType: string
+            let defaultFileName: string
 
-            if (typeof content === 'string') { // STRING ////////////////////////////////////////////////////////////////////
+            if (typeof content === 'undefined') { // EMPTY /////////////////////////////////////////////////////////////
+                defaultStatus = 204
+            } else if (typeof content === 'string') { // STRING ////////////////////////////////////////////////////////
                 defaultContentType = 'text/html; charset=UTF-8'
                 body = content
-            } else if (content instanceof Document) { // DOCUMENT ///////////////////////////////////////////////////////////
+            } else if (content instanceof Document) { // DOCUMENT //////////////////////////////////////////////////////
                 defaultContentType = 'text/html; charset=UTF-8'
                 body = new XMLSerializer().serializeToString(content)
-            } else if (content instanceof Response) { // RESPONSE ///////////////////////////////////////////////////////////
+            } else if (content instanceof Response) { // RESPONSE //////////////////////////////////////////////////////
                 defaultStatus = content.status
                 defaultContentType = content.headers.get('content-type') ?? ''
                 defaultHeaders = content.headers
                 body = content.arrayBuffer()
-            } else if (content instanceof File) { // FILE ///////////////////////////////////////////////////////////////////
+            } else if (content instanceof File) { // FILE //////////////////////////////////////////////////////////////
                 defaultContentType = content.type
                 defaultFileName = content.name
                 body = content
-            } else if (isBody(content)) { // BODY ///////////////////////////////////////////////////////////////////
+            } else if (content instanceof FormData) { // FORM DATA /////////////////////////////////////////////////////
+                defaultContentType = 'multipart/form-data'
                 body = content
-            } else { // JSON ///////////////////////////////////////////////////////////////////////////////////////////////
+            } else if (content instanceof URLSearchParams) { // URLENCODED /////////////////////////////////////////////
+                defaultContentType = 'application/x-www-form-urlencoded; charset=utf-8'
+                body = content
+            } else if (isBody(content)) { // BODY //////////////////////////////////////////////////////////////////////
+                defaultContentType = 'application/octet-stream'
+                body = content
+            } else { // JSON ///////////////////////////////////////////////////////////////////////////////////////////
+                defaultContentType = 'application/json'
                 body = JSON.stringify(content)
             }
 
             headers = customHeaders ? new Headers(customHeaders) : defaultHeaders
-            if (!headers.has('content-type')) headers.append('content-type', defaultContentType)
+            if (!headers.has('content-type') && defaultContentType) headers.append('content-type', defaultContentType)
             if (fileName) defaultFileName = fileName
             if (download || defaultFileName) {
                 let defaultContentDisposition = `${download ? 'attachment' : 'inline'};`
-                if (fileName) defaultContentDisposition += ` filename*=${this.encode(fileName)}`
+                if (defaultFileName) defaultContentDisposition += ` filename*=${this.encode(defaultFileName)}`
                 if (!headers.has('content-disposition')) headers.append('content-disposition', defaultContentDisposition)
             }
 
             let headerWhitelist = Array.from(headers).map(h => h[0]).join(', ')
             headers.set('httprelay-proxy-headers', headerWhitelist) // Whitelisting headers that must be passed to client
             headers.set('httprelay-proxy-status', `${status ?? defaultStatus}`)
+            if (this.wSecret) headers.set('httprelay-wsecret', this.wSecret)
 
             return Promise.resolve(body)
                 .then(b => (<SerRespInit> {
--- public/route.ts
+++ public/route.ts
@@ -1,17 +1,25 @@
 /// <reference path="handler.ts" />
 
 namespace HttpRelay.Proxy {
-    class Route {
+    export class Route {
         private readonly methodRe: RegExp
         private readonly pathRe: RegExp
+        private readonly pathDepth: number
 
-        constructor(public readonly method: string, public readonly path: string, private readonly handler: Handler) {
+        constructor(
+            public readonly method: string,
+            public readonly path: string,
+            private readonly handler: Handler
+        ) {
             this.methodRe = RegExp(method)
             this.pathRe = RegExp(path)
+            this.pathDepth = this.path.split('/').length
         }
 
-        public toString() {
-
+        public compare(r: Route): number {
+            let result = r.pathDepth - this.pathDepth
+            if (result == 0) result = r.path.length - this.path.length
+            return result
         }
     }
 }
